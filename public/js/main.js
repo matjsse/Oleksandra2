60a200781af19074aff30966bb1bb87a1a1eac29
Andrey Petrov <petrov.development@gmail.com>
2014-07-12 19:30:13 -0400

    jquery functions added

--- public/js/main.js
+++ public/js/main.js
@@ -1,25 +1,98 @@
 /**
+ * javascript basics
+ * jquery basics
+ * handlebars basics
  * Created by matisse on 7/10/2014.
  */
-$(function () {
+/*$(function () {
     var defaultContent = "This is Hello World by JQuery"
     //var homeContent = "this is home";
-    var aboutContent = '\<div class="testClass"\>this is about\</div\>';
+    var about = Handlebars.templates['about'];
     var home = Handlebars.templates['home']();
+    var work_mindandbody = Handlebars.templates['work_mindandbody']();
 
-    $("body").css({color: 'red'});
+    $(".page").html(home);
 
-    $(".nav-home").click(function () {
-        $(".content").html(home);
+    $(".maintitle").click(function () {
+        console.log("main title");
+        $(".page").html(home);
     });
 
-    $(".nav-about").click(function () {
-        $(".content").html(aboutContent);
+    $(".element1").click(function () {
+        $(".page").html(work_mindandbody);
     });
 
     $(".nav-contacts").click(function(){
         $(".content").html(home);
     });
 
+});*/
+
+$ (function () {
+
+    var pages = initPages();
+    hide(pages);
+    pages[0].show();
+    var currentPage = pages[0];
+
+    $('.nav-portfolio').click(function(){
+        //hide(pages);
+        currentPage.hide();
+        currentPage = pages[0];
+        currentPage.show();
+        //pages[0].show();
+    });
+
+    $('.nav-about').click(function(){
+        currentPage.hide();
+        currentPage = pages[0];
+        currentPage.show();
+    });
+
+
+
+    $('.nav-contacts').click(function(){
+        currentPage.hide();
+        currentPage = pages[1];
+        currentPage.show();
+    });
+
+    $('.element1').click(function(){
+        currentPage.hide();
+        currentPage = pages[2];
+        currentPage.show();
+    });
+
+
+    function hide(pages) {
+        pages.forEach(function(page) {
+            page.hide();
+        })
+    }
+
+
+    function initPages() {
+        var about = Handlebars.templates['about']();
+        var home = Handlebars.templates['home']();
+        var work_mindandbody = Handlebars.templates['work_mindandbody']();
+
+        var page1 = $(".page1");
+        page1.html(home);
+        var page2 = $(".page2");
+        page2.html(about);
+        var mindbody = $(".page3");
+        mindbody.html(work_mindandbody);
+
+        var pages = [];
+
+        pages[0] = page1;
+        pages[1] = page2;
+        pages[2] = mindbody;
+
+        return pages;
+    }
+
 });
+
+
 
